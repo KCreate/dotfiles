38defc77032b9b8da316ce2efebf101d69ea0017
Leonard Schütz <hello@leonardschuetz.ch>
2025-05-04 10:42:07 +0200

    yeet

--- .config.fish
+++ .config.fish
@@ -1,15 +1,13 @@
 # Set some variables
-set -gx CPATH /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/ $CPATH
 set -gx CHARLYVMDIR ~/Documents/code/KCreate/charly-vm
-set -gx NODE_PATH /usr/local/lib/node_modules
-
-set -gx PATH /usr/local/opt/llvm/bin $PATH
 set -gx PATH /opt/homebrew/bin $PATH
 
 # Disable the fish welcome greeting
 set fish_greeting
 
-# Load some scripts
+set GPG_TTY (tty)
+
+# hub extends the git command to work together more nicely with GitHub
 eval (hub alias -s)
 
 # Configure default fish path coloring
@@ -17,10 +15,10 @@ set -g fish_color_cwd green
 
 # Custom prompt
 function fish_prompt
-    # set_color $fish_color_cwd
-    # printf '%s' (prompt_pwd)
+    set_color $fish_color_cwd
+    printf '%s' (prompt_pwd)
     set_color yellow
-    printf '$ '
+    printf ' $ '
     set_color normal
 end
 
@@ -50,7 +48,7 @@ function fish_right_prompt
   set is_dirty (command git status --porcelain 2>/dev/null)
   if test "$branch" != ""
       printf "("
-      set_color blue
+      set_color green
       printf '%s' $branch
       if test "$is_dirty" != ""
           printf '*'
@@ -80,16 +78,28 @@ abbr --add code "cd ~/Documents/code"
 abbr --add desk "cd ~/Desktop"
 abbr --add dotfiles "cd ~/Documents/code/KCreate/dotfiles"
 abbr --add cdcharly "cd ~/Documents/code/KCreate/charly-vm"
+abbr --add cdcharlyrust "cd ~/Documents/code/KCreate/charly-rust"
+abbr --add cdjit "cd ~/Documents/code/KCreate/charly-jit"
 abbr --add .. "cd .."
 
 # Shorthand aliases for some often used commands
 alias l "ls -A1"
+alias ll "ls -lah"
 alias hexdump "hexdump -Cv"
 alias vim "nvim"
 alias g "git"
+alias gs "git s"
+alias gd "git diff"
+alias gdc "git diff --cached"
+alias gp "git push"
+alias gc "git commit"
 alias rg "rg --hidden -i"
 alias clip "pbcopy"
-alias vimfishconfig "vim ~/Documents/code/KCreate/dotfiles/.config.fish"
+
+function vimfishconfig
+  vim ~/Documents/code/KCreate/dotfiles/.config.fish
+  fishreload
+end
 
 function fishreload
   source ~/Documents/code/KCreate/dotfiles/.config.fish
@@ -118,10 +128,16 @@ function cd
   l;
 end
 
+# Create a new directory and immediately navigate to it
+function cdnew
+  mkdir -p $argv
+  builtin cd $argv
+end
+
 # Clone a repository from github into a folder
 function clone
   git clone git@github.com:$argv[1] "/Users/leonard/Documents/code/"$argv[1];
-  code;
+  cd "/Users/leonard/Documents/code";
   cd $argv[1];
 end
 
